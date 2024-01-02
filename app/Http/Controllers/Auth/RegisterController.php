d3b4213f83d4a07bed314cb7631ac72726e217b2
kevokyforce <37656333+kevokyforce@users.noreply.github.com>
2024-01-02 23:13:39 +0300

    Update RegisterController.php

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -22,7 +22,7 @@ class RegisterController extends Controller
     |
     */
 
-    use RegistersUsers;
+    use RegistersUsers = RouteServiceProvider::MAIN;
 
     /**
      * Where to redirect users after registration.
