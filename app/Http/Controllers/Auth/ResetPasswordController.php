bc042ce91e79d64422e6befc4ec16ac273e78dca
kevokyforce <37656333+kevokyforce@users.noreply.github.com>
2024-01-05 11:20:10 +0300

    Update ResetPasswordController.php

--- app/Http/Controllers/Auth/ResetPasswordController.php
+++ app/Http/Controllers/Auth/ResetPasswordController.php
@@ -26,5 +26,5 @@ class ResetPasswordController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = RouteServiceProvider::MAIN;
+    protected $redirectTo = RouteServiceProvider::HOME;
 }
